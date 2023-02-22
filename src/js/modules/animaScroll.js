c736611ad57ad1a0ab932e17df7c4ab83f9b6212
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-02-21 23:05:26 -0300

    animate scroll

---
+++ src/js/modules/animaScroll.js
@@ -0,0 +1,23 @@
+export default function animaScroll() {
+  const sections = document.querySelectorAll('[data-scroll="scroll"]');
+  
+  if (sections.length) {
+    const windowTop = window.innerHeight * 0.6;
+ 
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionVisible = (sectionTop - windowTop) < 0;
+
+        if(sectionVisible) {
+          section.classList.add('active');
+        } else {
+          section.classList.remove('active');
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
 import menuMobile from "./modules/mobile-menu.js";
+import animaScroll from "./modules/animaScroll.js";
 
+animaScroll();
 menuMobile();
